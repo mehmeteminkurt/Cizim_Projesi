e674406215f9b4aea90c090b0eedc74436c6d5aa
Mehmet Emin Kurt <m.eminkurt@outlook.com>
2023-01-23 21:06:26 +0300

    Proje dosyası ekle.

---
+++ CizimDersi1/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿
+namespace CizimDersi1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cizbtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cizbtn
+            // 
+            this.cizbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cizbtn.Location = new System.Drawing.Point(961, 466);
+            this.cizbtn.Name = "cizbtn";
+            this.cizbtn.Size = new System.Drawing.Size(116, 63);
+            this.cizbtn.TabIndex = 0;
+            this.cizbtn.Text = "Çiz";
+            this.cizbtn.UseVisualStyleBackColor = true;
+            this.cizbtn.Click += new System.EventHandler(this.cizbtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1130, 565);
+            this.Controls.Add(this.cizbtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cizbtn;
+    }
+}
+
---
+++ CizimDersi1/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CizimDersi1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cizbtn_Click(object sender, EventArgs e)
+        {
+            Graphics g = this.CreateGraphics();//this deyimi formu işaret eder.
+         
+            g.DrawString("MEHMET EMİN KURT", new Font("Arial", 30), Brushes.Red, 200, 200);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
